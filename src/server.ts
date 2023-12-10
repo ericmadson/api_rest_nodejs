e58b9e0c4d9de3389a3d909bf8a2dd41e159fb8b
EricM <ericmadson67@hotmail.com>
2023-12-10 19:04:29 -0300

    database created

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+import crypto from "node:crypto";
+import { knex } from "./database";
+import { env } from "./env";
+
+const app = fastify();
+
+app.get("/hello", async () => {
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de Teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
+});
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });
