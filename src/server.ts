7e55774e1e13f4158d3c025b666b69971ac094f3
unknown <ericmadson67@hotmail.com>
2023-12-13 11:06:55 -0300

    transactions routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
-app.get("/hello", async () => {
-  const transaction = await knex("transactions")
-    .insert({
-      id: crypto.randomUUID(),
-      title: "Transação de Teste",
-      amount: 1000,
-    })
-    .returning("*");
-
-  return transaction;
+app.register(cookie);
+app.register(transactionsRoutes, {
+  prefix: "transactions",
 });
 
 app
